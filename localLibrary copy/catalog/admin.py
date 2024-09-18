165ff17ab95d4e2c6cd185d7935b4399cbd909cb
sydthomason <156731151+sydthomason@users.noreply.github.com>
2024-09-18 10:46:50 -0500

    Delete localLibrary copy directory

--- localLibrary copy/catalog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book, Author, Genre, BookInstance
-
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
-
